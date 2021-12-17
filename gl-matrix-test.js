fbf0819cf0419f8cead6bdef856a5c496c6ebbf5
IIMrFreemanII <nikolay.diahovets@gmail.com>
2021-12-18 01:09:59 +0200

    gl-matrix

--- gl-matrix-test.js
+++ gl-matrix-test.js
@@ -1,25 +1,37 @@
-const { mat4, vec3 } = require('gl-matrix');
+const { mat4, vec3, glMatrix, quat } = require('gl-matrix');
 const Benchmark = require('benchmark');
 
 const suite = new Benchmark.Suite;
 let someMatrix = mat4.create();
 const translation = vec3.fromValues(1, 1, 1);
+const rotation = quat.create();
+quat.fromEuler(rotation, 30, 45, 23);
+const scale = vec3.fromValues(1, 1, 1);
 
-suite
-    .add('new identity matrix', function () {
-        mat4.translate(someMatrix, someMatrix, translation);
-        someMatrix = mat4.create();
-    })
-    .add('set to identity', function () {
-        mat4.translate(someMatrix, someMatrix, translation);
-        mat4.identity(someMatrix);
-    })
+glMatrix.setMatrixArrayType(Float32Array);
 
-    .on('cycle', function (event) {
-        console.log(String(event.target));
-    })
-    .on('complete', function () {
-        console.log('Fastest is ' + this.filter('fastest').map('name'));
-    })
-    // run async
-    .run({ 'async': true });
+// suite
+//     .add('rotation', function () {
+//         mat4.fromRotationTranslationScale(someMatrix, rotation, translation, scale);
+//     })
+//     // .add('set to identity', function () {
+//     //     mat4.fromRotationTranslationScale(someMatrix, someMatrix, translation);
+//     // })
+//     .on('cycle', function (event) {
+//         console.log(String(event.target));
+//     })
+//     .on('complete', function () {
+//         console.log('Fastest is ' + this.filter('fastest').map('name'));
+//     })
+//     // run async
+//     .run({ 'async': true });
+
+const matrixData = new Float32Array(5 * 16);
+const mat1 = new Float32Array(matrixData.buffer, 0 * 16 * 4, 16);
+
+mat1.forEach((_, i, arr) => {
+    arr[i] = 3;
+});
+
+console.log('matrixData', matrixData);
+console.log('mat1', mat1);
